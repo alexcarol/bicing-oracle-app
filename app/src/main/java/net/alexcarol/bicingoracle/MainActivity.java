7a9fe8b9266771ff97920baf49c2adcb97dd29cf
Alex Carol <alex.carol.c@gmail.com>
2016-02-12 12:49:28 +0100

    Created project

---
+++ app/src/main/java/net/alexcarol/bicingoracle/MainActivity.java
@@ -0,0 +1,18 @@
+package net.alexcarol.bicingoracle;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.HashMap;
+import java.util.Timer;
+import java.util.logging.Logger;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
