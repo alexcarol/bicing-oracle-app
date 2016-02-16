a036de3fef7bc8ab4ec1fbdd6dc16665abffcced
Alex Carol <alex.carol.c@gmail.com>
2016-02-16 13:23:47 +0100

    Created map

--- app/src/main/java/net/alexcarol/bicingoracle/MainActivity.java
+++ app/src/main/java/net/alexcarol/bicingoracle/MainActivity.java
@@ -43,7 +43,6 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("hour", hour);
                 intent.putExtra("minute", minute);
 
-
                 startActivity(intent);
             }
         });
