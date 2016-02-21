9c576a1b4ccd3ff9a74122b70b969a8ba5eb083b
Alex Carol <alex.carol.c@gmail.com>
2016-02-21 11:51:20 +0100

    Map zooms towards plaça catalunya and long click on it goes to the old main activity

--- app/src/main/java/net/alexcarol/bicingoracle/ChooseLocationActivity.java
+++ app/src/main/java/net/alexcarol/bicingoracle/ChooseLocationActivity.java
@@ -1,5 +1,10 @@
 package net.alexcarol.bicingoracle;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -30,8 +35,34 @@ public class ChooseLocationActivity extends FragmentActivity implements OnMapRea
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng barcelona = new LatLng(41.3909624, 2.22059);
+        LatLng barcelona = new LatLng(41.387148, 2.170122);
         mMap.addMarker(new MarkerOptions().position(barcelona).title("Marker in Barcelona"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(barcelona));
+
+        // TODO zoom depending on the stations
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(barcelona, 12));
+
+        enableCurrentLocation();
+
+        final Activity thisActivity = this;
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                startActivity(new Intent(thisActivity, MainActivity.class));
+            }
+        });
+    }
+
+    private void enableCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
     }
 }
