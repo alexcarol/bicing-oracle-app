7c10ff5fc114b0b20f08b552752cd9cbf8104593
Alex Carol <alex.carol.c@gmail.com>
2016-03-12 17:18:27 +0100

    Implemented api result parsing

--- app/src/main/java/net/alexcarol/bicingoracle/BicingOracleApi.java
+++ app/src/main/java/net/alexcarol/bicingoracle/BicingOracleApi.java
@@ -1,17 +1,44 @@
 package net.alexcarol.bicingoracle;
 
+import android.support.annotation.NonNull;
 import com.google.android.gms.maps.model.LatLng;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.SimpleTimeZone;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 class BicingOracleApi {
     public StationState[] getBicingData(long timestamp, LatLng position) {
-        return new StationState[]{
-                new StationState("Address 1", 1, 5, new LatLng(12, 13)),
-                new StationState("Address 2", 2, 3, new LatLng(12, 13)),
-        };
+        String requestResult = getFakeApiResult();
+        try {
+            JSONObject jsonObject = new JSONObject(requestResult);
+            JSONArray stations = jsonObject.getJSONArray("stations");
+
+            int length = stations.length();
+            StationState[] stationStates = new StationState[length];
+
+            for (int i = 0; i < length; ++i) {
+                JSONObject jsonStationState = stations.getJSONObject(i);
+
+                stationStates[i] = new StationState(
+                    jsonStationState.getString("address"),
+                    jsonStationState.getInt("slots"),
+                    jsonStationState.getInt("bikes"),
+                    new LatLng(jsonStationState.getDouble("lat"), jsonStationState.getDouble("lon"))
+                );
+
+            }
+
+            return stationStates;
+        } catch (JSONException e) {
+            ExceptionLogger.getInstance().log("BicingOracleApi", "Error parsing json", e);
+
+            return new StationState[]{};
+        }
+    }
+
+    @NonNull
+    private String getFakeApiResult() {
+        return "{\"stations\":[{\"address\":\"Gran via 123\", \"slots\":2, \"bikes\":7, \"lat\":12.2, \"lon\":12.1},{\"address\":\"Gran via 144\", \"slots\":1, \"bikes\":5, \"lat\":122, \"lon\":12.2}]}";
     }
 }
---
+++ app/src/main/java/net/alexcarol/bicingoracle/ExceptionLogger.java
@@ -0,0 +1,19 @@
+package net.alexcarol.bicingoracle;
+
+import android.util.Log;
+
+public class ExceptionLogger {
+    private static ExceptionLogger ourInstance = new ExceptionLogger();
+
+    public static ExceptionLogger getInstance() {
+        return ourInstance;
+    }
+
+    private ExceptionLogger() {
+    }
+
+    public void log(String tag, String message, Exception e) {
+        Log.e(tag, message, e);
+        // TODO implement some logging mecanism (log to server???)
+    }
+}
